9993c81f23ada067fbf6f22b3b09e1db4160589d
Javi Fontan <jfontan@gmail.com>
2011-12-10 00:07:26 +0100

    cleanup

--- server/main.rb
+++ server/main.rb
@@ -37,41 +37,11 @@ get '/e' do
     send_file 'static/index.html'
 end
 
-# assets
 =begin
 get '/new' do
     send_file 'static/index.html'
 end
 
-
-
-get %r{^.*/js/jquery.js$} do
-    send_file 'server/assets/jquery.js'
-end
-
-get %r{^.*/js/helpers.js$} do
-    send_file 'server/assets/helpers.js'
-end
-
-get %r{^.*/js/glsl.js$} do
-    send_file 'static/js/glsl.js'
-end
-
-get %r{^.*/js/codemirror.js$} do
-    send_file 'static/js/codemirror.js'
-end
-
-get %r{^.*/css/codemirror.css$} do
-    send_file 'static/css/codemirror.css'
-end
-
-get %r{^.*/css/default.css$} do
-    send_file 'static/css/default.css'
-end
-
-get %r{^.*/js/lzma.js$} do
-    "\n"
-end
 
 get %r{^/(\d+)(/(\d+))?$} do
     send_file 'static/index.html'
@@ -87,26 +57,6 @@ get %r{/item/(\d+)([/.](\d+))?} do
     end
 
     $glsl.get_code_json(code_id, version_id)
-end
-
-post %r{^/(new)$} do
-    body=request.body.read
-
-    $glsl.save_new_effect(body)
-end
-
-
-post  %r{^/(\d+)(/(\d+))?$} do
-    code_id=params[:captures][0].to_i
-    body=request.body.read
-
-    $glsl.save_version(code_id, body)
-
-    code=$glsl.get_code(code_id)
-
-    version=code['versions'].length-1
-
-    "#{code_id}/#{version}"
 end
 
 post '/e' do
