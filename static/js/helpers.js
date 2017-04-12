a592e95212bc91cac61c488667e1465a83e83c82
Xavier Ho <contact@xavierho.com>
2017-04-12 22:49:11 +1000

    Added WebGL 1.0 backwards support for mobile phones and older devices

--- static/js/helpers.js
+++ static/js/helpers.js
@@ -7,7 +7,7 @@ function initialize_compressor() {
 function initialize_helper() {
 }
 
-function load_url_code() {
+function load_url_code( lzma_compressed_code ) {
 	if ( window.location.hash ) {
 
 		var hash = window.location.hash.substr( 1 );
@@ -30,7 +30,7 @@ function load_url_code() {
 	} else {
 
 		// This is the default code to be loaded
-		readURL( '5d00000100a90200000000000000119d88a75623ed1f97decafdbca9f60d5d9aea5c15090d6b99d1bf278123255f7071406b0031147756a95f17500c89f6bb5b982f2996ce0eeec087a302f677faa8ff536d4904bd9e7deb9045d178189362a624333f49df388b60d0d4a174e5d774706548f6322bbca185d84070b143366137ea50788b60c695e83c44b476a2ca324430e11650bb06cc89bc7a19bfd7b93c1a5d382895d9db4865ff7514599a0c6ef3e5e4b232372ee947a42811132b7fa30d635c41c253def1996f6bf984204573e8dc43594c7c9446628b75e9b3ec3c79e14df44b5644785e0ec9ab4ad858a873a001868a7a49ab5d9ad3c7dd40b28e4dc1a4965078b9ce03a8ff5e2fd312cf7031476ad29a4edf64fe07de6907e00913a59f97834d305b9b8c4ffa85b049' );
+		readURL( lzma_compressed_code );
 
 	}
 }
